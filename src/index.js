f607d1c95d58799659ec3a081e3d2e664eebd566
Danifanton <danielrxd0912@gmail.com>
2023-05-27 21:12:20 -0400

    Install dependencies, create a project folder structure, create initial files and a basic frontend

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+
+const path = require("path");
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile("./renderer/index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
